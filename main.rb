892b9a2c4dd23eb49f62535cb8e012695350cd48
tsayen <anatoly.sayenko@gmail.com>
2016-02-22 21:26:54 +0100

    main

--- main.rb
+++ main.rb
@@ -1,4 +1,17 @@
+require './ui.rb'
+require './model.rb'
+
+puts '-------------------------'
 sinks = `pactl list short sinks`
 sinks.each_line do |line|
     puts line
 end
+puts '-------------------------'
+
+model = SwitchModel.new
+SwitchUi.new(model)
+
+model.add_sink Sink.new('sink1', 'Network')
+model.add_sink Sink.new('sink2', 'Local')
+
+Gtk.main
