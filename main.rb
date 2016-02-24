57c0860fa389b9a2b4a91ee85e637aeb03370831
tsayen <anatoly.sayenko@gmail.com>
2016-02-24 07:12:59 +0100

    read sinks from pactl

--- main.rb
+++ main.rb
@@ -1,17 +1,23 @@
 require './ui.rb'
 require './model.rb'
 
-puts '-------------------------'
-sinks = `pactl list short sinks`
-sinks.each_line do |line|
-    puts line
+def read_sinks
+    lines = `pactl list sinks | grep -e 'Name' -e 'Description'`.lines
+    sinks = []
+    while lines.size > 0
+        id = lines.shift.sub(/Name:/, '').strip
+        name = lines.shift.sub(/Description:/, '').strip
+        sinks.push Sink.new(id, name)
+    end
+    sinks
 end
-puts '-------------------------'
 
 model = SwitchModel.new
 SwitchUi.new(model)
 
-model.add_sink Sink.new('sink1', 'Network')
-model.add_sink Sink.new('sink2', 'Local')
+read_sinks.each do |sink|
+    model.add_sink sink
+    puts 'adding sink ' + sink.inspect
+end
 
 Gtk.main
