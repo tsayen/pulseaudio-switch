70ee1b8caeeee42042114ddc062baf2fccd2369b
tsayen <anatoly.sayenko@gmail.com>
2016-07-24 18:09:40 +0200

    minor changes

--- lib/model.rb
+++ lib/model.rb
@@ -1,18 +1,12 @@
 module AudioSwitch
   class Model
     def initialize(pactl)
-      @sinks = {}
       @pactl = pactl
     end
 
     def watch(&block)
       @update = block
       @update.call
-    end
-
-    def add_sink(sink)
-      @sinks[sink.id] = sink
-      @sink_added.call(sink) if @sink_added
     end
 
     def select_sink(sink_id)
